b389373846d03070b6c214f081be9cf2e46a28f6
mandeep <singh.mr.1996@gmail.com>
2021-06-24 20:07:42 +0530

    Added: uc1_Snake_And_Ladder_Played_Single_Player_Start_Position_0

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,9 +1,14 @@
 package com.bridgelabz;
 
-public class SnakeAndLadder {
-
-	public static void main(String[] args) {
-System.out.println("Hello BridgeLabz");
+public class SnakeAndLadder 
+{
+	public static void main(String[] args)
+	{
+		int Position = 0;
+		System.out.println("Welcome to Snake and Ladder Simulator");
+		System.out.println("Your Starting Position is : " +Position);
+		
 	}
+	
 
 }
