f45cfe5e6eca711cbe10a0fe3ec94fe62ca2f9ae
mandeep <singh.mr.1996@gmail.com>
2021-06-24 20:18:43 +0530

    Added: Uc3_Check_Option_NoPlay_Ladder_Or_Snake

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,18 +1,33 @@
 package com.bridgelabz;
-
+import java.util.*;
 public class SnakeAndLadder 
 {
 	public static void main(String[] args)
 	{
 		int Position = 0;
-		System.out.println("Welcome to Snake and Ladder Simulator");
-		System.out.println("Your Starting Position is : " +Position);
-		Dice();
-	}
-		public static void Dice()
+		Scanner Scan = new Scanner( System.in );
+		System.out.println("your position is " +Position);
+		int Dice = (int)Math.floor(Math.random()*10) % 6+1;
+		System.out.println("Your Dice Value is : " +Dice);
+		Position = Position + Dice;
+		int Check = (int)Math.floor(Math.random() * 10) % 3+1;
+		System.out.println("You Chose Case Number Is: "+Check);
+		switch (Check) 
 		{
-			int Dice = (int)Math.floor(Math.random()*10)%6+1;
-			System.out.println("Your Dice Value is : " +Dice);
+		case 1:
+			//Position = Position + Dice;
+			System.out.println("your Dice Position is : " +Position);
+			break;
+		case 2:
+			Position = Position - Dice;
+			System.out.println("your Dice Position is : " +Position);
+			break;	
+		case 3:
+			System.out.println("Player No Play Stay In The Same Position : "+Position);
+			break;
+		default:
+			break;
+		}
 		
 	}
 	
