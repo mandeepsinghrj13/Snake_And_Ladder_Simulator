56bd9d8f65ca58adef60504be6631af8699daac0
mandeep <singh.mr.1996@gmail.com>
2021-06-24 20:23:40 +0530

    Added: Uc4_Player_Reaches_The_Wining_Position_100

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,32 +1,42 @@
 package com.bridgelabz;
-import java.util.*;
+//import java.util.*;
 public class SnakeAndLadder 
 {
 	public static void main(String[] args)
 	{
 		int Position = 0;
-		Scanner Scan = new Scanner( System.in );
+		//int Position1 = 0;
+		//While((Position<=99)&&(Position1<=99))
+		while(Position <= 99) 
+		{
+		//Scanner Scan = new Scanner( System.in );
 		System.out.println("your position is " +Position);
 		int Dice = (int)Math.floor(Math.random()*10) % 6+1;
+		Position = (Position + Dice);
 		System.out.println("Your Dice Value is : " +Dice);
-		Position = Position + Dice;
 		int Check = (int)Math.floor(Math.random() * 10) % 3+1;
 		System.out.println("You Chose Case Number Is: "+Check);
-		switch (Check) 
+		switch (Check)  
 		{
 		case 1:
 			//Position = Position + Dice;
 			System.out.println("your Dice Position is : " +Position);
 			break;
 		case 2:
-			Position = Position - Dice;
+			Position = (Position - Dice);
 			System.out.println("your Dice Position is : " +Position);
 			break;	
 		case 3:
+			Position = (Position - Dice);
+			if(Position<0)
+			{
+				Position = 0;
+			}
 			System.out.println("Player No Play Stay In The Same Position : "+Position);
 			break;
-		default:
-			break;
+			
+		}
+		
 		}
 		
 	}
